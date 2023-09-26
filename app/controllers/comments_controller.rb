255936831bb9ee520cf1c82a0a6b3e495110278e
yahir91 <yahir.cardona@gmail.com>
2023-09-26 10:59:28 -0600

    feat: created ui for comments and added real time render

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:create]
 
-    before_action :set_post, only: [:create]
-    def create
-        @comment = @post.comments.create(user: current_user, body: params[:comment_body])
-    end
+  def create
+    @comment = @post.comments.create(user: current_user, body: params[:comment_body])
 
-    def destroy
-        @comment = Comment.find(params[:id])
-        if @comment.user === current_user
-            @comment.destroy
-        end
-        
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "post#{@post.id}comments",
+          partial: "posts/post_comments",
+          locals: { post: @post },
+        )
+      end
     end
+  end
 
-    private
-    def set_post
-        @post = Post.find(params(:post_id))
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.user === current_user
+      @comment.destroy
     end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 end
