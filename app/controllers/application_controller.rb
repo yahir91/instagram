5db38999fb7ad94b6d24cd4c0b0e6e8bb2aaeeca
yahir91 <yahir.cardona@gmail.com>
2023-09-26 13:41:17 -0600

    feat: updated edit profile settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-    protected
+  protected
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up , keys: [:full_name, :username, :phone_number ])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :username, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :phone_number, :full_name, :profile_pic, :bio, :private])
+  end
 end
