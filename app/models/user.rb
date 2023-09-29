c1db35ff9a3b0253575d9d23f7c2959600fef28b
yahir91 <yahir.cardona@gmail.com>
2023-09-29 08:53:56 -0600

    feat: create follow model and relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,19 @@ class User < ApplicationRecord
   has_many :comments
 
   has_one_attached :profile_pic
+
+  has_many :follow_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "followed_id"
+  has_many :accepted_recieved_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "followed_id"
+  has_many :accepted_sent_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "follower_id"
+  has_many :followers, through: :accepted_recieved_requests, source: :user
+  has_many :followed, through: :accepted_sent_requests, source: :user
+
+
+  # has_many :recieved_requests, class_name: "Follow", foreign_key: "followed_id"
+  # has_many :send_requests, class_name: "Follow", foreign_key: "follower_id"
+
+
+  # has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
+
+
 end
