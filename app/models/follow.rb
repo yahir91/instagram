e15e197cc43d18cc0d0281dc2a152454b26df27f
yahir91 <yahir.cardona@gmail.com>
2023-09-29 09:31:21 -0600

    chore: create helper functions for user and follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,8 @@
 class Follow < ApplicationRecord
     belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
     belongs_to :followed, class_name: 'User', foreign_key: 'followed_id'
+
+    def accept
+        self.update(accepted: true)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,8 @@ class User < ApplicationRecord
   has_many :follow_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "followed_id"
   has_many :accepted_recieved_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "followed_id"
   has_many :accepted_sent_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "follower_id"
-  has_many :followers, through: :accepted_recieved_requests, source: :user
-  has_many :followed, through: :accepted_sent_requests, source: :user
+  has_many :followers, through: :accepted_recieved_requests, source: :follower
+  has_many :followings, through: :accepted_sent_requests, source: :followed
 
 
   # has_many :recieved_requests, class_name: "Follow", foreign_key: "followed_id"
@@ -25,5 +25,13 @@ class User < ApplicationRecord
 
   # has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
 
+  def follow(user)
+    Follow.create(follower: self, followed: user)
+  end
 
+  def unfollow(user)
+    self.accepted_sent_requests.find_by(followed: user)&.destroy    
+  end
 end
+
+
