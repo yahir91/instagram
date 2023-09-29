09085e424cc5679a05961fc7b5b13c896c2639f5
yahir91 <yahir.cardona@gmail.com>
2023-09-29 09:54:44 -0600

    chore: created check privacy function

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,8 +1,15 @@
 class Follow < ApplicationRecord
-    belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
-    belongs_to :followed, class_name: 'User', foreign_key: 'followed_id'
+  before_create :check_privacy
+  belongs_to :follower, class_name: "User", foreign_key: "follower_id"
+  belongs_to :followed, class_name: "User", foreign_key: "followed_id"
 
-    def accept
-        self.update(accepted: true)
-    end
+  def accept
+    self.update(accepted: true)
+  end
+
+  private
+
+  def check_privacy
+    self.accepted = true unless self.followed.private
+  end
 end
