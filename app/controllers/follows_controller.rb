ceee466144c7c37fb7a6f90d8d18a5d3ba2ab6f5
yahir91 <yahir.cardona@gmail.com>
2023-09-29 13:13:47 -0600

    fix: typo

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,30 +1,31 @@
 class FollowsController < ApplicationController
-  before_action :set_user, only: [:folllow, :unfollow, :cancel_requests]
-  before_action :set_follow_req, only: [:accept_follow, :decline_follow, :cancel_requests]
+  before_action :set_user, only: [:follow, :unfollow, :cancel_request]
+  before_action :set_follow_req, only: [:accept_follow, :decline_follow]
 
   def follow
     current_user.follow(@user)
-    redirect_back(fallback: root_path)
+    redirect_back(fallback_location: root_path)
+
   end
 
-  def cancel_requests
+  def cancel_request
     current_user.cancel_request(@user)
-    redirect_back(fallback: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   def unfollow
     current_user.unfollow(@user)
-    redirect_back(fallback: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   def accept_follow
     @follow_req.accept
-    redirect_back(fallback: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   def decline_follow
     @follow_req.destroy
-    redirect_back(fallback: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   private
