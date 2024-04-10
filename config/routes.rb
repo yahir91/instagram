fbf81363132fd4cc2a9f8d3d9c944de9af23d7f1
yahir91 <yahir.cardona@gmail.com>
2024-04-10 16:43:50 -0600

    feat: added searching for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   resources :comments, only: [:create, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
